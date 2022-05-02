9cd0462e5effd21fe27fffad37fbea3753cddddb
Juho Soivio <soivio.juho@gmail.com>
2022-05-02 11:48:36 +0300

    tietokannasta tietoa saldo-ikkunaan

--- bankautomat/saldo.cpp
+++ bankautomat/saldo.cpp
@@ -1,11 +1,19 @@
+#include "mysingleton.h"
 #include "saldo.h"
 #include "ui_saldo.h"
+#include <QTimer>
 
 Saldo::Saldo(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Saldo)
 {
     ui->setupUi(this);
+
+    timer = new QTimer(this);
+
+    connect(timer, SIGNAL(timeout()), this, SLOT(showData()));
+        timer->start(100);
+
 
     QString s = QString::number(saldo, 'saldo', 2);
 
@@ -16,3 +24,27 @@ Saldo::~Saldo()
 {
     delete ui;
 }
+
+void Saldo::showData()
+{
+    MySingleton *pMySingleton = MySingleton::getInstance();
+
+    ui->label_Tunnus->setText(pMySingleton->getTunnus());
+    ui->label_Nimi->setText(pMySingleton->getNimi());
+    ui->label_Osoite->setText(pMySingleton->getLahiOsoite());
+    ui->saldoLabel->setText(pMySingleton->getSaldo());
+}
+
+
+
+
+
+
+
+
+
+void Saldo::on_CloseSaldo_clicked()
+{
+    this->close();
+}
+
